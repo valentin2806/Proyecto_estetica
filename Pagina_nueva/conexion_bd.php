96d9f9ce62c5041189f62a54d4e3dc4f89a62dda
valentin2806 <130018171+valentin2806@users.noreply.github.com>
2023-08-07 21:03:25 -0300

    Delete Pagina_nueva directory

--- Pagina_nueva/conexion_bd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Conexión a la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "estetica";
-
-$conexion = new mysqli($servername, $username, $password, $dbname);
